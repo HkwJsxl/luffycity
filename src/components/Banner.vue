6f951c8c98b57053b087c05910a2dc6d244a41e7
韩科伟 <hkwJsxl@gmail.com>
2023-01-24 16:23:18 +0800

    初始化

---
+++ src/components/Banner.vue
@@ -0,0 +1,46 @@
+<template>
+
+  <div id="banner">
+    <el-carousel height="400px">
+      <el-carousel-item v-for="(item, index) in banner_list" :key="index">
+        <router-link :to="item.link">
+          <img :src="item.image" :alt="item.name">
+        </router-link>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  data() {
+    return {
+      banner_list: []
+    }
+  },
+  created() {
+    //当banner组件一创建，就向后台发请求，拿回轮播图数据
+    this.$axios.get(this.$settings.base_url + '/home/banner/').then(response => {
+      console.log('res_data', response.data)
+      this.banner_list = response.data;
+    }).catch(() => {
+
+    })
+  },
+
+}
+</script>
+
+<style scoped>
+.el-carousel__item {
+  height: 400px;
+  min-width: 1200px;
+}
+
+.el-carousel__item img {
+  height: 400px;
+  margin-left: calc(50% - 1920px / 2);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div className="home">
+    <Header/>
+    <Banner/>
+    <div>
+      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    </div>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+//导入头部组件
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import Banner from '@/components/Banner'
+
+export default {
+  name: "Home",
+  data() {
+    return {}
+  },
+  created() {
+    this.$axios.get(this.$settings.base_url + '/home/home/').then(response => {
+      console.log(response.data)  //response.data才是真正后台返回的数据
+    }).catch(errors => {
+      console.log(errors)
+    })
+  },
+  components: {
+    Header,
+    Footer,
+    Banner,
+  }
+}
+</script>
