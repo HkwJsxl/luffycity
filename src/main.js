857f0eab51037f068e6cf99fb652a3437f5cec88
hkwJsxl <hkwJsxl@gmail.com>
2023-02-06 14:45:55 +0800

    UPDATE

--- src/main.js
+++ src/main.js
@@ -27,8 +27,6 @@ Vue.prototype.$axios = axios;
 Vue.prototype.$cookies = cookies;
 
 // vue-video播放器
-// import 'video.js/dist/video-js.css';
-// import VideoPlayer from 'vue-video-player'
 require('video.js/dist/video-js.css');
 require('vue-video-player/src/custom-theme.css');
 import VideoPlayer from 'vue-video-player'
